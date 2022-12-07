cd1f22479e10c240f4cd289d4e21f95d7c0aa830
psalmistcom <dadewale9@gmail.com>
2022-12-07 09:23:09 +0100

    added state

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,13 @@
+import { CssBaseline, ThemeProvider } from "@mui/material"
+import { createTheme } from "@mui/material"
+import { useSelector } from "react-redux"
+import { themeSettings } from "theme"
+
 function App() {
+  const mode = useSelector((state) => state.global.mode)
+  const theme = useMemo(()=> createTheme(themeSettings(mode)), [mode])
   return (
-    <div className="app">
-    </div>
+    <div className="app"></div>
   );
 }
 
