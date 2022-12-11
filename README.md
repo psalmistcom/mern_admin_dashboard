3c897270ebc7a9c56826244eb8b7971902feaf1c
psalmistcom <dadewale9@gmail.com>
2022-12-11 23:42:09 +0100

    transaction backend

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 > By Adewale David 
 
-Front End: ReactJS
-State Management: Redux
-Database: NoSQL (MongoDB)
-Backend: Nodejs, Express
+## Front End: ReactJS
+## State Management: Redux
+## Database: NoSQL (MongoDB)
+## Backend: Nodejs, Express
--- server/controllers/client.js
+++ server/controllers/client.js
@@ -1,6 +1,7 @@
 import Product from '../models/Product.js'
 import ProductStat from '../models/ProductStat.js'
 import User from '../models/User.js'
+import Transaction from '../models/Transaction.js'
 
 export const getProducts = async (req, res) => {
     try {
