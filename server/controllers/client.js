3f6bfdf1c8ec760f6656c86a0242c35d9cc7aecf
psalmistcom <dadewale9@gmail.com>
2022-12-12 21:04:03 +0100

    geography backend

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -2,6 +2,7 @@ import Product from '../models/Product.js'
 import ProductStat from '../models/ProductStat.js'
 import User from '../models/User.js'
 import Transaction from '../models/Transaction.js'
+import getCountryISO3 from "country-iso-2-to-3"
 
 export const getProducts = async (req, res) => {
     try {
@@ -35,7 +36,7 @@ export const getCustomers = async (req, res) => {
 	}
   };
 
-  
+//   get transaction controller 
 export const getTransactions = async (req, res) => {
 	try {
 	  // sort should look like this: { "field": "userId", "sort": "desc"}
@@ -70,6 +71,33 @@ export const getTransactions = async (req, res) => {
 		transactions,
 		total,
 	  });
+	} catch (error) {
+	  res.status(404).json({ message: error.message });
+	}
+  };
+
+  //get customers controller 
+export const getGeography = async (req, res) => {
+	try {
+		const users = await User.find()
+
+		const mapLocations = users.reduce((acc, { country }) =>{
+			const countryISO3 = getCountryISO3(country)
+
+			if(!acc[countryISO3]){
+				acc[countryISO3] = 0
+			}
+			acc[countryISO3]++
+			return acc
+		}, {})
+
+		const formattedLocations = Object.entries(mapLocations).map(
+			([country, count]) =>{
+				return { id: country, value: count}
+			}
+		)
+
+		res.status(200).JSON(formattedLocations)
 	} catch (error) {
 	  res.status(404).json({ message: error.message });
 	}
