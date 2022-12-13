109b984a953d46482063500b6707cd35dbdec1b7
psalmistcom <dadewale9@gmail.com>
2022-12-13 13:40:09 +0100

    geaography scene started

---
+++ client/src/scenes/geography/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import {
+    Box,
+    useTheme,
+} from "@mui/material";
+import Header from "components/Header";
+import { useGetGeographyQuery } from "state/api";
+
+
+const Geography = () => {
+    return (
+        <div>
+            Geography
+        </div>
+    )
+}
+
+export default Geography
