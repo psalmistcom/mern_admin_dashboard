a06a5bbfea50d349729adf6abdea10cf39f4dbef
psalmistcom <dadewale9@gmail.com>
2022-12-14 22:55:27 +0100

    overviiew chart done

--- client/src/scenes/overview/index.jsx
+++ client/src/scenes/overview/index.jsx
@@ -1,16 +1,10 @@
 import React, { useState } from "react";
-import {
-    Box,
-    FormControl,
-    MenuItem,
-    InputLabel,
-    Select
-} from "@mui/material";
-import OverviewChart from "components/OverviewChart"
+import { FormControl, MenuItem, InputLabel, Box, Select } from "@mui/material";
 import Header from "components/Header";
+import OverviewChart from "components/OverviewChart";
 
-const Sales = () => {
-    const [view, setView] = useState("units")
+const Overview = () => {
+    const [view, setView] = useState("units");
 
     return (
         <Box m="1.5rem 2.5rem">
@@ -30,4 +24,4 @@ const Sales = () => {
     )
 }
 
-export default Sales
+export default Overview
