fbe458dd8d78281183074bd99eae26f190854e32
psalmistcom <dadewale9@gmail.com>
2022-12-16 19:26:19 +0100

    all scenes done

--- server/index.js
+++ server/index.js
@@ -16,7 +16,15 @@ import Product from './models/Product.js'
 import ProductStat from './models/ProductStat.js'
 import  Transaction from './models/Transaction.js'
 import  OverallStat from './models/OverallStat.js'
-import { dataUser, dataProduct, dataProductStat, dataTransaction, dataOverallStat } from './data/index.js'
+import AffiliateStat from "./models/AffiliateStat.js";
+import {
+  dataUser,
+  dataProduct,
+  dataProductStat,
+  dataTransaction,
+  dataOverallStat,
+  dataAffiliateStat,
+} from "./data/index.js";
 
 
 // CONFIG
@@ -45,6 +53,7 @@ mongoose.connect(process.env.MONGO_URL, {
     app.listen(PORT, () => console.log(`SERVER RUNNING ON port ${PORT}`));
 
     // Add data ones
+    AffiliateStat.insertMany(dataAffiliateStat);
     // OverallStat.insertMany(dataOverallStat)
     // Product.insertMany(dataProduct)
     // ProductStat.insertMany(dataProductStat)
