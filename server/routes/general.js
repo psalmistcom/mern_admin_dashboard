2f3df9a22ed9444899c854695abedd122d0e8e65
psalmistcom <dadewale9@gmail.com>
2022-12-16 18:03:25 +0100

    general route BE

--- server/routes/general.js
+++ server/routes/general.js
@@ -1,7 +1,9 @@
-import express from 'express'
-import { getUser } from '../controllers/general.js'
+import express from "express";
+import { getUser, getDashboardStats } from "../controllers/general.js";
 
 const router = express.Router();
 
 router.get("/user/:id", getUser);
-export default  router
+router.get("/dashboard", getDashboardStats);
+
+export default router;
